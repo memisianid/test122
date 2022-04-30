2546b524d193a03420ae3f6ad5c1bb53d612af6e
Kaushall Vimalarajah <v.kaushi4@gmail.com>
2022-04-30 23:27:24 +0200

    test

---
+++ src/main/java/com/codeusingjava/service/ImgGalleryService.java
@@ -0,0 +1,32 @@
+package com.codeusingjava.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.codeusingjava.model.ImgGallery;
+import com.codeusingjava.repo.ImgGalleryRepo;
+
+
+
+@Service
+public class ImgGalleryService {
+
+	@Autowired
+	private ImgGalleryRepo imageGalleryRepository;
+	
+	public void saveImage(ImgGallery imgGallery) {
+		imageGalleryRepository.save(imgGallery);	
+	}
+
+	public List<ImgGallery> getAllActiveImages() {
+		return imageGalleryRepository.findAll();
+	}
+
+	public Optional<ImgGallery> getImageById(Long id) {
+		return imageGalleryRepository.findById(id);
+	}
+}
+
---
+++ src/test/java/com/codeusingjava/SpringBootImageGalleryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeusingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootImageGalleryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
